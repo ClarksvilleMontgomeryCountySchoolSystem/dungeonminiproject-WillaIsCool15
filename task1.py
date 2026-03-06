61fb9dbb41695cc915222efb35d1a374d1a95856
WillaIsCool15 <willa.conway206@students.cmcss.net>
2026-03-05 19:25:10 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,7 +1,8 @@
-torch_lit=True
-if torch_lit:
+torch_lit=False
+if not torch_lit:
 outcome="Doom: oh no I can't see it's so dark!"
     else
 outcome="Flicker: Yay I can see again!"
 
 print(outcome)
+
